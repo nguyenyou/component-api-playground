4ee6bb4de16ca59459e5f36cda41334901c07b1d
nguyenyou <tunt081295@gmail.com>
2025-09-30 02:05:41 +0700

    🚢 ship it

--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -7,7 +7,7 @@ case class App() {
   def apply(): HtmlElement = {
     div(
       cls("h-screen w-screen flex justify-center items-center"),
-      Button(color = _.Red)(),
+      Button(_.Red)(),
       Button(color = _.Green)(),
       Button(color = _.Blue)(),
     )
