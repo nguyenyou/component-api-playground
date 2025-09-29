d44d2f354692ff35cec370356e7c916dada8d5d2
nguyenyou <tunt081295@gmail.com>
2025-09-29 21:12:08 +0700

    first

--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -1,12 +1,22 @@
 package www
 
 import com.raquo.laminar.api.L.*
+import scala.language.implicitConversions
+import scala.compiletime.summonInline
 
 case class App() {
+  enum Color {
+    case Red, Green, Blue
+  }
+  type ColorMod = Color.type => Color
+  val mycolor: ColorMod = _.Red
+  val colorobj = mycolor(Color)
+
   def apply(): HtmlElement = {
+    println(colorobj)
     div(
       cls("h-screen w-screen flex justify-center items-center"),
-      "Hello, World!"
+      "Hello, Worldddd!"
     )
   }
 }
--- www/src/www/Main.scala
+++ www/src/www/Main.scala
@@ -6,8 +6,8 @@ import com.raquo.laminar.api.L.*
 object Main {
   @main
   def run(): Unit = {
-    // render(dom.document.getElementById("app"), App()())
-    MainReactApp()
+    render(dom.document.getElementById("app"), App()())
+    // MainReactApp()
   }
 
   def hello(): String = "Hello World"
