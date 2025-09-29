2b9ef2182fe537e7f0802dce1fd445f64d2d24df
nguyenyou <tunt081295@gmail.com>
2025-09-29 21:36:40 +0700

    🚢 ship it

--- www/src/www/Button.scala
+++ www/src/www/Button.scala
@@ -1,12 +1,19 @@
 package www
 
 import com.raquo.laminar.api.L.*
+import scala.compiletime.summonInline
+import scala.language.implicitConversions
+
+transparent inline given modToValue[C <: Singleton, T]: Conversion[C => T, T] =
+  (f: C => T) => f(summonInline[ValueOf[C]].value)
 
 case class Button(color: Button.ColorMod) {
   val colorobj = color(Button.Color)
+  val obj: Button.Color = color
   
   def apply() = {
     println(colorobj)
+    println(obj)
     button("Button")
   }
 }
