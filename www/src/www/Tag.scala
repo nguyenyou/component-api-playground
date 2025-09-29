b292ecba4f330b46123eb9673d311724daf299e9
nguyenyou <tunt081295@gmail.com>
2025-09-29 21:48:25 +0700

    🚢 ship it

--- www/src/www/Tag.scala
+++ www/src/www/Tag.scala
@@ -4,8 +4,8 @@ import com.raquo.laminar.api.L.*
 import scala.compiletime.summonInline
 // Generic conversion: apply a function from a singleton type `C` to its singleton value.
 // Works for enums, case classes with companions, plain `object`s, etc.
-transparent inline given modToValue[C <: Singleton, T]: Conversion[C => T, T] =
-  (f: C => T) => f(summonInline[ValueOf[C]].value)
+given modToValue[C <: Singleton, T](using v: ValueOf[C]): Conversion[C => T, T] with
+  def apply(f: C => T): T = f(v.value)
   
 case class Tag(color: Tag.TagMod) {
   val obj: Tag.Color = color
